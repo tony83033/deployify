2b89e431a1bcb2cbe29cc49f9b42b7a9cb839fbd
Sumit pal <tonystark83033@gmail.com>
2024-03-24 16:00:00 +0530

    core:[fix:[fix filePath]

--- build-server/script.js
+++ build-server/script.js
@@ -1,5 +1,6 @@
 const {exec} = require('child_process');
 const fs = require('fs');
+const path = require('path');
 const {S3Client, PutObjectCommand} = require('@aws-sdk/client-s3');
 const mime = require('mime-types');
 // create a new client of s3
@@ -7,8 +8,8 @@ const mime = require('mime-types');
 const s3Client = new S3Client({
     region: 'ap-south-1',
     credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+        accessKeyId: 'AKIAU6GDWNLEQECIJW2F',
+        secretAccessKey: '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'
     }
 })
 
@@ -17,7 +18,7 @@ async function init(){
     console.log("Executing script.js to build code");
     const outDirPath = path.join(__dirname, 'output');
 
-    const p = exec(`cd ${outDirPath} && npm run install && npm run build`);
+    const p = exec(`cd ${outDirPath} && npm install && npm run build`);
     p.stdout.on('data',function(data){
         console.log(data.toString());
     })
@@ -26,20 +27,22 @@ async function init(){
         console.log("Error", data.toString());
     })
 
-    p.stdout.on('close', async function(data){
+    p.stdout.on('close', async function(){
         // after completing build process upload dist || output dir to s3
         console.log("Build completed");
 
         const distDirPath = path.join(__dirname, 'output','dist');
         // No we need to read all the content of dist dir we are using fs module for it
-        const distDirContent = fs.readdirSync(distDirPath,{recursive: true}); // it will return an array
+        const distDirContent = fs.readdirSync(distDirPath, {recursive: true}); // it will return an array
 
-        for(const filePath of distDirContent){
+        for(const file of distDirContent){
+            const filePath = path.join(distDirPath, file);
             // check if file is directory
-            if(filePath.lstatSync().isDirectory()){
+            if(fs.lstatSync(filePath).isDirectory()){
                 continue;
             }
 
+            console.log("uploading",filePath);
             const command = new PutObjectCommand({
                 Bucket: 'deployify-project',
                 Key: `__output/${PROJECT_ID}/${filePath}`,
@@ -49,10 +52,12 @@ async function init(){
             });
 
             // Now send to command to s3
-            await S3Client.send(command);
-
+            await s3Client.send(command);
+            console.log("uploaded",filePath);
 
         }
         console.log('Done uploading to s3');
     })
 }
+
+init();
