acd1a2d1e7360e6caf8d4630e50b9f9302ccbfdd
Sumit pal <tonystark83033@gmail.com>
2024-03-26 22:18:01 +0530

    feat:[Add redis to publish blog]

---
+++ api-server/index.js
@@ -0,0 +1,97 @@
+const express = require('express');
+const {generateSlug} = require('random-word-slugs');
+const Redis = require('ioredis');
+const {Server} = require('socket.io')
+const {ECSClient,RunTaskCommand} = require('@aws-sdk/client-ecs');
+const app = express();
+const PORT = 9000;
+
+const redisSubcriber = new Redis('rediss://default:AVNS_omCddpi9Pkp3uTnAcel@redis-deployify-tonystark83033-c11f.a.aivencloud.com:23302');
+
+const io = new Server(
+    {
+        cors:'*'
+    }
+);
+
+io.listen(9001,()=>{
+    console.log(`Socket Server listening on port 9001`);
+});
+
+io.on('connection', (socket)=>{
+    socket.on('subscribe',channel =>{
+        socket.join(channel);
+        socket.emit('message',`Joined to ${channel}`);
+    })
+})
+
+const ecsClient = new ECSClient({
+    region:'ap-south-1',
+    credentials: {
+        accessKeyId: 'AKIAU6GDWNLEQECIJW2F',
+        secretAccessKey: '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'
+    }
+})
+
+const config = {
+    CLUSTER:"arn:aws:ecs:ap-south-1:339712830153:cluster/deployifyCluster",
+    TASK_DEFINITION: "arn:aws:ecs:ap-south-1:339712830153:task-definition/deployify-builder-task",
+}
+
+app.use(express.json());
+
+app.post('/project', async(req,res)=>{
+    const projectSlug = generateSlug();
+    const {gitUrl} = req.body;
+    //run a container docker container
+    const command = new RunTaskCommand({
+        cluster: config.CLUSTER,
+        taskDefinition: config.TASK_DEFINITION,
+        launchType: 'FARGATE',
+        count: 1,
+        networkConfiguration: {
+            awsvpcConfiguration: {
+                subnets: [
+                    'subnet-059782448d033ed26','subnet-08fdd16eeace3b911','subnet-0d655f6c6d206772f'
+                ],
+                securityGroups:['sg-016fd9f6cf8775a6d'],
+                assignPublicIp: 'ENABLED'
+            }
+        },
+        overrides: {
+            containerOverrides: [
+                {
+                    name: 'builder-image',
+                    environment:[
+                        {
+                            name:'GIT_REPO_URL',value:gitUrl
+                        },
+                        {
+                            name:'PROJECT_ID',value:projectSlug
+                        }
+                    ]
+                }
+            ]
+        }
+    });
+
+    await ecsClient.send(command);
+    return res.status(200).json({status:'queued',data:{projectSlug,url:`http://${projectSlug}.localhost:8000`}});
+})
+
+
+
+ async function initRedisSubcribe(){
+    console.log('starting redis subcribe');
+    console.log('Subscribed to logs....');
+    redisSubcriber.psubscribe('logs:*');
+    redisSubcriber.on('pmessage',(pattern,channel,message)=>{
+        io.to(channel).emit('message',message);
+        console.log(message);
+    })
+}
+
+initRedisSubcribe();
+app.listen(PORT,()=>{
+    console.log('API SERVER LISTENING ON PORT',PORT);
+})
--- build-server/script.js
+++ build-server/script.js
@@ -3,6 +3,13 @@ const fs = require('fs');
 const path = require('path');
 const {S3Client, PutObjectCommand} = require('@aws-sdk/client-s3');
 const mime = require('mime-types');
+const Redis = require('ioredis');
+
+
+
+const redis = new Redis('rediss://default:AVNS_omCddpi9Pkp3uTnAcel@redis-deployify-tonystark83033-c11f.a.aivencloud.com:23302');
+
+
 // create a new client of s3
 
 const s3Client = new S3Client({
@@ -14,27 +21,35 @@ const s3Client = new S3Client({
 })
 
 const PROJECT_ID = process.env.PROJECT_ID;
+
+function publishLogs(log){
+    redis.publish(`logs:${PROJECT_ID}`,JSON.stringify({log}));
+}
 async function init(){
     console.log("Executing script.js to build code");
+    publishLogs('Projec Build started...');
     const outDirPath = path.join(__dirname, 'output');
 
     const p = exec(`cd ${outDirPath} && npm install && npm run build`);
     p.stdout.on('data',function(data){
         console.log(data.toString());
+        publishLogs(data.toString());
     })
 
     p.stdout.on('error', function(data){
         console.log("Error", data.toString());
+        publishLogs(`Error: ${data.toString()}`);
     })
 
     p.stdout.on('close', async function(){
         // after completing build process upload dist || output dir to s3
         console.log("Build completed");
+        publishLogs('Build Completed');
 
         const distDirPath = path.join(__dirname, 'output','dist');
         // No we need to read all the content of dist dir we are using fs module for it
         const distDirContent = fs.readdirSync(distDirPath, {recursive: true}); // it will return an array
-
+        publishLogs("Starting upload to s3");
         for(const file of distDirContent){
             const filePath = path.join(distDirPath, file);
             // check if file is directory
@@ -42,7 +57,8 @@ async function init(){
                 continue;
             }
 
-            console.log("uploading",filePath);
+            console.log("uploading...",filePath);
+            publishLogs(`Uploading ${file} to s3`);
             const command = new PutObjectCommand({
                 Bucket: 'deployify-project',
                 Key: `__output/${PROJECT_ID}/${file}`,
@@ -54,9 +70,11 @@ async function init(){
             // Now send to command to s3
             await s3Client.send(command);
             console.log("uploaded",filePath);
+            publishLogs(`Uploaded ${file} to s3`);
 
         }
         console.log('Done uploading to s3');
+        publishLogs('Done uploading to s3');
     })
 }
 
