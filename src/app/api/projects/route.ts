0c76e82ed9cb1618b630d16d1fbb2378948017eb
Sumit pal <tonystark83033@gmail.com>
2024-03-29 02:31:10 +0530

    fix:[run prettier]

--- src/app/api/projects/route.ts
+++ src/app/api/projects/route.ts
@@ -1,29 +1,31 @@
-import {NextResponse, NextRequest} from 'next/server';
-import {z} from 'zod';
-import {PrismaClient} from '@prisma/client';
-import { generateSlug } from 'random-word-slugs';
+import { NextResponse, NextRequest } from "next/server";
+import { z } from "zod";
+import { PrismaClient } from "@prisma/client";
+import { generateSlug } from "random-word-slugs";
 const prisma = new PrismaClient();
 export async function POST(request: NextRequest) {
-    const schema = z.object({
-        name: z.string(),
-        gitURL: z.string()
-    })
-    const body = await request.json();
-    const safeParseResult = schema.safeParse(body);
-    if(!safeParseResult.success) {
-        return NextResponse.json({error: safeParseResult.error},{status: 400});
-    }
+  const schema = z.object({
+    name: z.string(),
+    gitURL: z.string(),
+  });
+  const body = await request.json();
+  const safeParseResult = schema.safeParse(body);
+  if (!safeParseResult.success) {
+    return NextResponse.json({ error: safeParseResult.error }, { status: 400 });
+  }
 
-    const {name, gitURL} = safeParseResult.data;
-    const project = await prisma.project.create({
-        data: {
-            name,
-            gitURL,
-            subdomain:generateSlug(),
-            customDomain:generateSlug(),
-        }
-    })
+  const { name, gitURL } = safeParseResult.data;
+  const project = await prisma.project.create({
+    data: {
+      name,
+      gitURL,
+      subdomain: generateSlug(),
+      customDomain: generateSlug(),
+    },
+  });
 
-        return NextResponse.json({status:"success",data:project},{status:200});
-    
+  return NextResponse.json(
+    { status: "success", data: project },
+    { status: 200 },
+  );
 }
