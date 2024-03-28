14a38f163ad61afda32ab493bbb392a88f95eece
Sumit pal <tonystark83033@gmail.com>
2024-03-29 02:16:22 +0530

    feat:[Add reverse proxy for s3]

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
+
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async rewrites() {
+        return [
+           {
+            source:"/project/:slug",
+            destination:"https://deployify-project.s3.ap-south-1.amazonaws.com/__output/:slug/index.html"
+           }
+        ]
+    },
+};
 
 export default nextConfig;
