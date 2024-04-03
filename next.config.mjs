66c0383af4a4ea58ab6f63c769de60b02fbc6ac3
Sumit pal <tonystark83033@gmail.com>
2024-04-04 03:52:08 +0530

    fix:[remove rewrite fun]

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: "/project/:slug",
-        destination:
-          "https://deployify-project.s3.ap-south-1.amazonaws.com/__output/:slug/index.html",
-      },
-    ];
-  },
+
+    
 };
 
 export default nextConfig;
